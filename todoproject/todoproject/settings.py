2e743e5eaba700fc56284ae2159b979e1a1f4d9a
DMprog <107614760+ITprogDM@users.noreply.github.com>
2022-10-03 16:50:56 +0300

    Update settings.py

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2t7e(gg%3)@m@r=navcqoc$yu2!*b6w*(k@4dmt%mybi=c9prp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
